3b30599760c1615bbe620964d30832dbbb44c51b
ssu.techture@gmail.com <ssu.techture@gmail.com>
2024-08-24 21:19:37 +0530

    added firebase and deployment code

---
+++ src/firebaseConfig.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Import Firestore if needed
+import { getAuth } from "firebase/auth"; // Import Auth if needed
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDvvPpSz1kNR_aDn9TJ1aAwhYrsEPQcrpM",
+  authDomain: "manage-todo-app.firebaseapp.com",
+  projectId: "manage-todo-app",
+  storageBucket: "manage-todo-app.appspot.com",
+  messagingSenderId: "116797045828",
+  appId: "1:116797045828:web:fac77f0c5d4c7ab017d011",
+  measurementId: "G-DW5M1ES047",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
